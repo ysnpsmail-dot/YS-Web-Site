8af11e4ac07f583ad815fa91186ef15b9a335428
ysnpsmail-dot <ysnpsmail@gmail.com>
2026-02-07 16:09:34 +0300

    Update Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-red-500">
+      <h1 className="text-4xl font-bold">HERO SECTION WORKS 🔥</h1>
+    </section>
+  );
+}
